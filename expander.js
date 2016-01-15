7f07f447982d1b171845a629a799963b0302cda5
Josh Marinacci <joshua@marinacci.org>
2016-01-14 22:08:36 -0800

    ignore case

--- expander.js
+++ expander.js
@@ -5,7 +5,8 @@ var app = express();
 var config = JSON.parse(fs.readFileSync("config.json").toString());
 
 app.get('*',function(req,res) {
-    if(config.mappings[req.url]) return res.redirect(config.mappings[req.url]);
+    var url = req.url.toLowerCase();
+    if(config.mappings[url]) return res.redirect(config.mappings[url]);
     res.status(404).send("invalid path " + req.url);
 });
 
